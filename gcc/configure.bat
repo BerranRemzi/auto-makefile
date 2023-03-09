7ce272bc23b2d9c6c1b3543402036bc1addb7c20
BerranRemzi <berranremzi@gmail.com>
2023-03-09 06:54:21 +0200

    Add xc8-cc example

---
+++ gcc/configure.bat
@@ -0,0 +1,3 @@
+set MAKE_PATH=D:\portapps\eclipse\msys64\mingw64\bin
+set MSYS_PATH=D:\portapps\eclipse\msys64\usr\bin
+set PATH=%MAKE_PATH%;%MSYS_PATH%;%PATH%
---
+++ xc8-cc/configure.bat
@@ -0,0 +1,6 @@
+@echo off
+set MAKE_PATH=D:\portapps\eclipse\msys64\mingw64\bin
+set MSYS_PATH=D:\portapps\eclipse\msys64\usr\bin
+set DFP_PATH="C:/Tools/Microchip/MPLABX/v6.00/packs/Microchip/PIC10-12Fxxx_DFP/1.5.61/xc8"
+set COMPILER_PATH="C:\Tools\Microchip\xc8\v2.40\bin"
+set PATH=%MAKE_PATH%;%MSYS_PATH%;$PATH%
